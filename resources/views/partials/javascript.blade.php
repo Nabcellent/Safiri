5f2c03d711427947321877c543c66995e894d5cd
Nabcellent <nabcellent.dev@gmail.com>
2021-11-06 22:10:57 +0300

    Added Javascript file.

---
+++ resources/views/partials/javascript.blade.php
@@ -0,0 +1,54 @@
+<script>
+    {{--    TOAST ALERT    --}}
+    @if (session('toast_info'))
+    toast({msg: '{{ session('toast_info') }}', type: 'info'});
+    @endif
+
+    @if (session('toast_success'))
+    toast({msg: '{{ session('toast_success') }}', type: 'success'});
+    @endif
+
+    @if (session('toast_warning'))
+    toast({msg: '{{ session('toast_warning') }}', type: 'warning'});
+    @endif
+
+    @if (session('toast_error') || session('toast_danger'))
+    toast({msg: '{{ session('toast_error') ?: session('toast_danger') }}', type: 'danger'});
+    @endif
+
+
+    {{--    SWEET ALERT    --}}
+    @if (session('sweet_info'))
+    sweet({msg: '{{ session('sweet_info') }}', type: 'info', title: 'NB:'});
+    @endif
+
+    @if (session('sweet_error'))
+    sweet({msg: '{{ session('sweet_error') }}', type: 'error'});
+    @endif
+
+    @if (session('sweet_warning'))
+    sweet({msg: '{{ session('sweet_warning') }}', type: 'warning'});
+    @endif
+
+    @if (session('sweet_success'))
+    sweet({msg: '{{ session('sweet_success') }}', type: 'success', title: 'GREAT!!'});
+    @endif
+
+
+    window.sweet = data => {
+        Swal.fire({
+            title: data.title ? data.title : 'Oops...',
+            text: data.msg,
+            icon: data.type
+        });
+    }
+
+    window.toast = data => {
+        Toastify({
+            text: data.msg,
+            duration: 7000,
+            close: true,
+            className: data.type,
+        }).showToast();
+    }
+</script>

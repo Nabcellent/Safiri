5eb09d9485e55d5103510b34e36ac019816fd3bb
136495 <ochiengeli2020@gmail.com>
2021-11-23 16:55:06 +0300

    Eli Init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run() {
         $this->call([
-//            UserSeeder::class,
-//            SettingSeeder::class,
+            UserSeeder::class,
+            SettingSeeder::class,
             PaymentMethodSeeder::class
         ]);
     }

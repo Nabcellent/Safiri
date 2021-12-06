bc67e0bc1681c91960fd84fca84cf4f089451a96
Nabcellent <nabcellent.dev@gmail.com>
2021-12-06 20:16:21 +0300

    App: Implemented search
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- resources/views/partials/filters.blade.php
+++ resources/views/partials/filters.blade.php
@@ -9,7 +9,8 @@
             <ul class="list-group list-group-flush">
                 @foreach($categories as $category)
                     <label class="list-group-item">
-                        <input class="form-check-input me-1 filter-check fil-category" type="checkbox" value="{{ $category->id }}">
+                        <input class="form-check-input me-1 filter-check fil-category" type="checkbox"
+                               value="{{ $category->id }}">
                         {{ ucwords(str_replace('_',' ', $category->title)) }}
                     </label>
                 @endforeach
@@ -18,16 +19,16 @@
             <h6 class="fw-bold mb-4 mt-5 text-uppercase">Price</h6>
             <div class="row">
                 <div class="col-12 form-group mb-2">
-                    <input type="text" class="js-range-slider" name="my_range" id="age_range" aria-label/>
+                    <input type="text" class="js-range-slider" name="my_range" id="price-range" aria-label/>
                 </div>
                 <div class="col-6 form-group">
                     <input type="number" min="0" value="2000"
-                           class="form-control form-control-sm bg-transparent text-center rounded-0 price_range"
+                           class="form-control form-control-sm bg-transparent text-center rounded-0 price-range"
                            style="border:none; border-bottom: 1px solid var(--safiri-green)" aria-label id="min_price">
                 </div>
                 <div class="col-6 form-group">
                     <input type="number" min="0" value="35000"
-                           class="form-control form-control-sm bg-transparent text-center rounded-0 price_range"
+                           class="form-control form-control-sm bg-transparent text-center rounded-0 price-range"
                            style="border:none; border-bottom: 1px solid var(--safiri-green)" aria-label id="max_price">
                 </div>
             </div>
@@ -36,7 +37,8 @@
             <ul class="list-group list-group-flush">
                 @foreach($vicinities as $vicinity)
                     <label class="list-group-item">
-                        <input class="form-check-input me-1 filter-check fil-vicinity" type="checkbox" value="{{ $vicinity->id }}">
+                        <input class="form-check-input me-1 filter-check fil-vicinity" type="checkbox"
+                               value="{{ $vicinity->id }}">
                         {{ $vicinity->vicinity }}
                     </label>
                 @endforeach
@@ -53,9 +55,9 @@
          * --------------------------------------------------------------------------------------   RANGE SELECTOR
          * */
 
-        let minPrice = $('#min_price'), maxPrice = $('#max_price')
+        let minPrice = $('#min_price'), maxPrice = $('#max_price'), priceRangeEl = $('.price-range');
 
-        let priceRange = $(".js-range-slider").ionRangeSlider({
+        let priceRangeSlider = $(".js-range-slider").ionRangeSlider({
             type: "double",
             skin: "sharp",
             min: 0,
@@ -72,14 +74,18 @@
             },
         }).data("ionRangeSlider");
 
-        $('.price_range').on('keyup', () => {
+        priceRangeEl.on('keyup', () => {
             if (minPrice.val() > 0 && maxPrice.val() < 50000)
-                priceRange.update({
+                priceRangeSlider.update({
                     from: minPrice.val(),
                     to: maxPrice.val()
                 });
         });
 
-        if (minPrice.val() && maxPrice.val()) priceRange.update({from: minPrice.val(), to: maxPrice.val()});
+        priceRangeEl.on('change', () => {
+            if (minPrice.val() > 10 && maxPrice.val() < 10000) getProducts()
+        });
+
+        if (minPrice.val() && maxPrice.val()) priceRangeSlider.update({from: minPrice.val(), to: maxPrice.val()});
     </script>
 @endpush
